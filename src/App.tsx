64a61e817d9a31b12f570d15c362a1c6741801cf
Samuelplim <samuel12nerd@gmail.com>
2021-11-10 22:13:23 -0400

    Problema solucionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Route } from "react-router-dom";
-//import GlobalStyles from "./styles/GlobalStyles";
-//<Route path="/Register" exact component={Register} />
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SignIn } from "./screens/SignIn";
 import { Register } from "./screens/Register";
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/">
-        <SignIn />
-      </Route>
-      <Route path="/Register">
-        <Register />
-      </Route>
-    </BrowserRouter>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route path="/Register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
